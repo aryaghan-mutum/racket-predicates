0bbd8bb8347bc97c124244a5dda2f452d0fba882
anuragmuthyam <anu.drumcoder@gmail.com>
2020-08-23 19:09:07 -0400

    refac list-predicates-test.rkt

--- test/list-predicates-test.rkt
+++ test/list-predicates-test.rkt
@@ -7,29 +7,32 @@
 (require rackunit)
 (require "../list/list-predicates.rkt")
 
+;; empty?
+(check-equal? (empty? '()) #t)
+(check-equal? (empty? '(pi)) #f)
+(check-equal? (empty? '("string")) #f)
+(check-equal? (empty? #t) #f)
+(check-equal? (empty? #f) #f)
+(check-equal? (empty? 'symbol) #f)
+
+;; zero-lst?
 (check-eqv? (zero-lst? '()) #t)
 (check-eqv? (zero-lst? (list 1 1 1 0 0 1)) #f)
 (check-eqv? (zero-lst? (list 1 1 0 1 1 0)) #f)
 (check-eqv? (zero-lst? (list 0 0 0 0 0 0)) #t)
 
+;; list?
+; (list? '(0))                                ;#t
+; (list? '(1 3 5 7))                          ;#t
+; (list? '(a b null '(1 2)))                  ;#t
+; (list? 23)                                  ;#f
+; (ist? '1)                                  ;#f
+; (list? (cons 1 2))                          ;#f
+; (list? (cdr '(1 2 3 4 5)))                  ;#t
+; (list? (cons 1 (cons 2 (cons 3 '()))))      ;#t
 
-
-(nil? '())      ;#t
-(nil? empty)    ;#t
-(nil? null)     ;#t
-(nil? '(1 2 3)) ;#f
-
-
-(my-list? '(0))                                ;#t
-(my-list? '(1 3 5 7))                          ;#t
-(my-list? '(a b null '(1 2)))                  ;#t
-(my-list? 23)                                  ;#f
-(my-list? '1)                                  ;#f
-(my-list? (cons 1 2))                          ;#f
-(my-list? (cdr '(1 2 3 4 5)))                  ;#t
-(my-list? (cons 1 (cons 2 (cons 3 '()))))      ;#t
-
-(member? 1 '(1 2 3))
-(member? 10 '(1 2 3))
-(member? 10 '())
-(member? 'e '(a b c d))
+;; member?
+(check-eqv? (member? 1 '(1 2 3)) #t)
+(check-eqv? (member? 10 '(1 2 3)) #f)
+(check-eqv? (member? 10 '()) #f)
+(check-eqv? (member? 'e '(a b c d)) #f)
